1f9d4c8b29c50789e5619b319b5e9d90e306e477
fiji370 <kabylake1070@gmail.com>
2017-02-23 07:29:52 -0600

    Uploaded version 1.0.1 ARC
    ARC can now get similar words in near the first keywords with the matchSnippets function.

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk import ne_chunk
 from nltk import pos_tag
+from nltk import sent_tokenize
+
+## Read the input document
+with open("INPUT.txt") as text:
+    inputDoc = text.read()
 
 ## Ask for the question
 litQ = raw_input("Please input your question: \n")
@@ -57,9 +62,46 @@ def preprocess(question):
     return both
 
 def matchSnippets(document, keywords):
-    pass
+    namedEntities = []
+    
+    together = []
+    
+    smartKeys = keywords
+    
+    firstMatches = [i for i in sent_tokenize(document) for x in word_tokenize(i) if x in smartKeys]
+    
+    
+    for i in firstMatches:
+        for x in ne_chunk(pos_tag(word_tokenize(i))):
+            try:
+               namedEntities.append(x.leaves())
+            except:
+                continue
+    
+    for i in namedEntities:
+        transientList = []
+        for x in i:
+            transientList.append(x[0])
+        together.append(transientList)
+    
+    together = [" ".join(i) for i in together]
+    nouns = together
+    
+    for i in firstMatches:
+        for x in pos_tag(word_tokenize(i)):
+            if "NN" in x[1] and x[0] not in " ".join(nouns):
+                nouns.append(x[0])
+    
+    for i in nouns:
+        smartKeys.append(i)
+    
+    smartKeys = [i for i in smartKeys if i.lower() not in set(stopwords.words('english')) and word_tokenize(document).count(i.lower()) < 50]
+    smartKeys = set(smartKeys)
+    smartKeys = list(smartKeys)
+    
+    return smartKeys
 
 def presentable(snippet):
     pass
-    
-print(preprocess(litQ))
+
+print matchSnippets(inputDoc, preprocess(litQ))
